3531890bc40aa7158a9f4f9064152a10252acd49
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-01 09:15:39 +0500

    Add TextBoxDecorator to change text box appearance if input is incorrect

---
+++ DbAuthApp.Registration/TextBoxDecorator.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DbAuthApp.Registration
+{
+    public class TextBoxDecorator
+    {
+        private TextBox _textBox;
+        private Brush _defaultBorderBrush;
+        
+        public TextBoxDecorator(TextBox textBox)
+        {
+            _textBox = textBox;
+            _defaultBorderBrush = textBox.BorderBrush;
+        }
+        
+        public void InputIsIncorrect(string message)
+        {
+            _textBox.BorderBrush = Brushes.Red;
+            _textBox.ToolTip = message;
+        }
+
+        public void InputIsCorrect()
+        {
+            _textBox.BorderBrush = Brushes.Green;
+            _textBox.ToolTip = "";
+        }
+
+        public void Reset()
+        {
+            _textBox.BorderBrush = _defaultBorderBrush;
+        }
+    }
+}
